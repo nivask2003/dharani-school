2df10f1254acae6f3a7f14db570c3f7be178a92a
Nivas Kumar S <nivask457@gmail.com>
2026-05-27 14:12:32 +0530

    File Updated

--- facilities.php
+++ facilities.php
@@ -24,6 +24,110 @@ include 'function.php';
     <?=template_header()?>
     <div class="page-banner-fac">
         <h1 class="banner-title">Facilities</h1>
+        
+    </div>
+    <div class="fac-1 py-5">
+        <div class="container">
+            <div class="heading">
+                <span class="badge"><i class="bi bi-mortarboard-fill"></i> Our Facilities</span>
+                <h2 class="section-title" style="font-weight: 700!important;">
+                What Parents Say About Us
+            </h2>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="box">
+                        <img src="Assets\Images\fac-1.webp" alt="" class="img-fluid">
+                        <h3 class="box-title mt-4">Smart Classroom</h3>
+                        <p class="box-desc">Sri Dharani International School offers smart classrooms with digital tools and interactive learning methods that improve creativity, participation, and conceptual understanding.</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="box">
+                        <img src="Assets\Images\fac-2.webp" alt="" class="img-fluid">
+                        <h3 class="box-title mt-4">Montessori Room</h3>
+                        <p class="box-desc">The Montessori Activity Room encourages hands-on learning, creativity, communication, confidence, motor skill development, and independent thinking in young learners.</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="box">
+                        <img src="Assets\Images\fac-3.webp" alt="" class="img-fluid">
+                        <h3 class="box-title mt-4">Kindergartner Space</h3>
+                        <p class="box-desc">Sri Dharani International School offers smart classrooms with digital tools and interactive learning methods that improve creativity, participation, and conceptual understanding.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row py-5">
+                <div class="col-md-4">
+                    <div class="box">
+                        <img src="Assets\Images\fac-4.webp" alt="" class="img-fluid">
+                        <h3 class="box-title mt-4">Science Laboratory</h3>
+                        <p class="box-desc">The school features advanced science laboratories supporting practical experiments, observation, research activities, and hands-on learning in Physics, Chemistry, and Biology.</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="box">
+                        <img src="Assets\Images\fac-5.webp" alt="" class="img-fluid">
+                        <h3 class="box-title mt-4">Computer Laboratory</h3>
+                        <p class="box-desc">The computer laboratory provides modern systems and digital resources that develop technological skills, computer knowledge, and digital literacy among students..</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="box">
+                        <img src="Assets\Images\fac-6.webp" alt="" class="img-fluid">
+                        <h3 class="box-title mt-4">Mathematics Laboratory</h3>
+                        <p class="box-desc">The Mathematics Laboratory encourages activity-based learning through models and demonstrations, improving logical thinking, problem-solving skills, and concept understanding.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row py-5">
+                <div class="col-md-4">
+                    <div class="box">
+                        <img src="Assets\Images\fac-7.webp" alt="" class="img-fluid">
+                        <h3 class="box-title mt-4">Central Library</h3>
+                        <p class="box-desc">The Central Library offers diverse academic resources that encourage reading habits, independent learning, knowledge development, and research among students.</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="box">
+                        <img src="Assets\Images\fac-8.webp" alt="" class="img-fluid">
+                        <h3 class="box-title mt-4">Sports Infrastructure</h3>
+                        <p class="box-desc">The school provides quality sports facilities promoting fitness, teamwork, discipline, physical development, and active participation in sports activities among students.</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="box">
+                        <img src="Assets\Images\fac-9.webp" alt="" class="img-fluid">
+                        <h3 class="box-title mt-4">CCTV Surveillance</h3>
+                        <p class="box-desc">The campus is monitored through CCTV surveillance systems ensuring student safety, discipline, security, and a protected educational environment.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row py-5">
+                <div class="col-md-4">
+                    <div class="box">
+                        <img src="Assets\Images\fac-10.webp" alt="" class="img-fluid">
+                        <h3 class="box-title mt-4">Transportation Facility</h3>
+                        <p class="box-desc">The school provides safe transportation facilities with maintained buses and experienced drivers, ensuring secure and comfortable student travel daily.</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="box">
+                        <img src="Assets\Images\fac-11.webp" alt="" class="img-fluid">
+                        <h3 class="box-title mt-4">School Canteen</h3>
+                        <p class="box-desc">The school canteen offers hygienic and nutritious food in a clean environment, promoting healthy eating habits and student wellbeing.</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="box">
+                        <img src="Assets\Images\fac-12.webp" alt="" class="img-fluid">
+                        <h3 class="box-title mt-4">Kindergarten Play Area</h3>
+                        <p class="box-desc">The Kindergarten Play Area encourages safe play activities that develop coordination, creativity, confidence, and social skills among children.</p>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
     </div>
     <?=template_footer()?>
      <!-- jQuery -->
